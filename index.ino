6c8d09d60e4954e4b7925bd53594afc2d1b9653f
Max Van Aken <mtvanaken@gmail.com>
2021-07-20 23:36:06 +0100

    Set  and timer button interface

--- index.ino
+++ index.ino
@@ -3,49 +3,86 @@
 #include "rgb_lcd.h"
 #include <Servo.h>
 
-#define BUTTON_PIN 12
+#define SET_BUTTON 11
+#define SECONDS_BUTTON 12
 
 Servo servo;
 rgb_lcd lcd;
 
-int counter = 10; 
-String timerState = "LOCKED";
+int counter = 0; 
+String timerState = "UNLOCKED";
 
 void setup() {
     Serial.begin(115200);
     lcd.begin(16, 2);
     servo.attach(9);
-    servo.write(0);
-    pinMode(BUTTON_PIN, INPUT);
+    servo.write(180);
+    pinMode(SET_BUTTON, INPUT);
+    pinMode(SECONDS_BUTTON, INPUT);
     delay(1000);
 }
 
 void loop() {
-    counter--;
-    Serial.println(counter);
-    Serial.println(digitalRead(BUTTON_PIN));
-    if (digitalRead(BUTTON_PIN) == HIGH) {
-        counter = counter + 5;
+    Serial.println(timerState);
+    Serial.println(digitalRead(SET_BUTTON));
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("JUUL is ");
+    lcd.print(timerState);
+    lcd.setCursor(0, 1);
+    lcd.print(counter);
+    lcd.print(" seconds left");
+
+    if(timerState == "LOCKED") {
+        if(counter < 1) {
+            timerState = "UNLOCKED";
+            servo.write(0);
+        }
+        else {
+            counter--;
+        }
     }
-    if (counter > 0) {
-        timerState = "LOCKED";
-        lcd.setCursor(0, 0);
-        lcd.print("JUUL is ");
-        lcd.print(timerState);
-        lcd.setCursor(0, 1);
-        lcd.print(counter);
-        lcd.print(" seconds left");
+    else if (timerState == "UNLOCKED") {
+        if(digitalRead(SET_BUTTON) == HIGH) {
+            timerState = "SET";
+        } 
     }
-    else if (counter <= 0) {
-        timerState = "UNLOCKED";
-        counter = 0;
-        lcd.setCursor(0, 0);
-        lcd.print("JUUL is ");
-        lcd.print(timerState);
-        lcd.setCursor(0, 1);
-        lcd.print(counter);
-        lcd.print(" seconds left");
-        servo.write(180);
+    else if (timerState == "SET") {
+        if(digitalRead(SECONDS_BUTTON) == HIGH) {
+            counter = counter + 5;
+        }
+        else if (digitalRead(SET_BUTTON) == HIGH && counter > 5) {
+            timerState = "LOCKED";
+            servo.write(0)
+        }
     }
+
+
+
+    // if (counter > 0) {
+    //     lcd.clear();
+    //     timerState = "LOCKED";
+    //     lcd.setCursor(0, 0);
+    //     lcd.print("JUUL is ");
+    //     lcd.print(timerState);
+    //     lcd.setCursor(0, 1);
+    //     lcd.print(counter);
+    //     lcd.print(" seconds left");
+    // }
+    // else if (counter <= 0) {
+    //     timerState = "UNLOCKED";
+    //     counter = 0;
+    //     lcd.clear();
+    //     lcd.setCursor(0, 0);
+    //     lcd.print("JUUL is ");
+    //     lcd.print(timerState);
+    //     lcd.setCursor(0, 1);
+    //     lcd.print(counter);
+    //     lcd.print(" seconds left");
+    //     servo.write(180);
+    //     if (digitalRead(BUTTON_PIN) == HIGH) {
+    //         counter = counter + 15;
+    //     }
+    // }
     delay(1000);
 }
